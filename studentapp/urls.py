c7f55f98ffadff884740da78288fae8c5cc84064
Nebula Geo Technologies <nebulageotechnologies@gmail.com>
2026-08-11 12:42:13 +0530

    Update student management login

--- studentapp/urls.py
+++ studentapp/urls.py
@@ -1,7 +1,8 @@
 from django .urls  import path
 from . import views
+from django.contrib.auth import views as auth_views
 urlpatterns = [
-   
+path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
  path('home/', views.home, name='home'),
  path('students/', views.student_list, name='student_list'),
  path('add/', views.add_student, name='add_student'),
