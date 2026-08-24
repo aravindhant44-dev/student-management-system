69b9b641198bc185594ca1f4389ac28879234aaa
Aravindhan T <aravindhant44@gmail.com>
2026-08-24 12:43:41 +0530

    Configure Neon PostgreSQL database

--- studentapp/urls.py
+++ studentapp/urls.py
@@ -1,14 +1,13 @@
-from django .urls  import path
+from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
-urlpatterns = [
-path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
- path('home/', views.home, name='home'),
- path('students/', views.student_list, name='student_list'),
- path('add/', views.add_student, name='add_student'),
- path('update/<int:pk>/', views.update_student, name='update_student'),
- path('delete/<int:pk>/', views.delete_student, name='delete_student'),
- path('search/', views.search_student, name='search_student'),
- path('logout/', views.logout_view, name='logout'),
 
+urlpatterns = [
+    path('', views.login_view, name='login'),
+    path('home/', views.home, name='home'),
+    path('students/', views.student_list, name='student_list'),
+    path('add/', views.add_student, name='add_student'),
+    path('update/<int:pk>/', views.update_student, name='update_student'),
+    path('delete/<int:pk>/', views.delete_student, name='delete_student'),
+    path('search/', views.search_student, name='search_student'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- studentproject/settings.py
+++ studentproject/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,14 +82,29 @@ WSGI_APPLICATION = 'studentproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import os
+
+if os.environ.get("POSTGRES_URL"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DATABASE"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": os.environ.get("POSTGRES_HOST"),
+            "PORT": "5432",
+            "OPTIONS": {
+                "sslmode": "require",
+            },
+        }
     }
-}
-
-
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
